9bfeb8e81017a700c93bc7495484c3074073eac0
Albert So <albertyso@gmail.com>
2020-07-14 23:17:55 -0400

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,23 @@
+functions
+
+can be simple as
+
+let a = function() { // this is anonymous
+}
+
+a // prints function: a
+
+also
+
+function a() {
+
+}
+
+a // prints function: a
+
+
+(function a() {
+  4
+})
+
+// returns 4, wrapping in parens causes it to evaluate
