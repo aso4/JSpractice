e7303d59d3618541bd2cfd8f6b70f437d48cf383
Albert So <albertyso@gmail.com>
2020-07-14 23:32:38 -0400

    Update practice.js
    function parameters

--- practice.js
+++ practice.js
@@ -21,3 +21,18 @@ a // prints function: a
 })
 
 // returns 4, wrapping in parens causes it to evaluate
+
+() => {} // this is also a function
+
+(() => { return 4 })() // wrapped and called
+
+// params
+let a = (value) => { return value }
+
+// params are optional, can still call a()
+
+// similarly
+let a = (value, valuetwo) => { return value + valuetwo }
+
+let arr = [5, 10, 15]
+a(...arr) // spread syntax
