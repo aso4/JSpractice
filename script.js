54b068de6d9318dab41b1b1579ef8122ac10e58e
Albert So <albertyso@gmail.com>
2020-07-18 23:51:16 -0400

    JS for script execution html

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+// document.write(document.styleSheets.length) adds "1" to document
+
+// one of three most powerful methods in js
+// overwrites style.css
+document.getElementsByTagName('h1')[0].style.color = 'red'
+
+document.write('bye')
