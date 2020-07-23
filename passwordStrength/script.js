db42b54cd7439a1e546c71f901d7dafa0423ddcf
aso4 <albertyso@gmail.com>
2020-07-22 23:55:52 -0400

    add conditions for password strength return values

--- passwordStrength/script.js
+++ passwordStrength/script.js
@@ -1,5 +1,16 @@
 function measureStrength() {
   let passwordElem = document.getElementById('password-input');
+  let strengthElem = document.getElementById('strength-level');
 
-  console.log(passwordElem.value);
+  let strengthLevel = 'Weak';
+
+  let password = passwordElem.value;
+
+  if (password.length >= 8 && (password.length < 12)) {
+    strengthLevel = 'Strong';
+  } else if (password.length >= 12) {
+    strengthLevel = 'Very Strong';
+  }
+
+  strengthElem.innerText = strengthLevel;
 }
